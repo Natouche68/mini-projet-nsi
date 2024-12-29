e398cde515dcaee4584e2fc55bcf00ec133d0ecc
Natouche68 <ngrossheny@gmail.com>
2024-12-29 18:46:28 +0100

    Create a basic home screen

--- main.py
+++ main.py
@@ -2,4 +2,29 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Mini-projet NSI")
+window.geometry("800x500")
+
+
+def home():
+    def start():
+        home_frame.destroy()
+
+    home_frame = tk.Frame(window)
+    home_frame.pack()
+
+    arrow_number_label = tk.Label(home_frame, text="Nombre de flèches :")
+    arrow_number_label.pack()
+    arrow_number_entry = tk.Spinbox(
+        home_frame, from_=1, to=30, increment=1, textvariable=tk.IntVar(value=9))
+    arrow_number_entry.pack()
+
+    is_trispot_entry = tk.Checkbutton(home_frame, text="Sur trispot")
+    is_trispot_entry.pack()
+
+    validate_button = tk.Button(home_frame, text="Commencer", command=start)
+    validate_button.pack()
+
+
+home()
+
 window.mainloop()
