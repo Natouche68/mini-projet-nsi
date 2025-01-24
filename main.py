a8f616542eda33fbe1573226f8ad722e8c1ea9fd
Natouche <ngrossheny@gmail.com>
2025-01-24 17:02:18 +0100

    Cours du 20 janvier

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ arrow_number = tk.IntVar(value=9)
 is_trispot = tk.BooleanVar(value=False)
 arrows_coordinates = []
 current_volley = 1
+overview_scroll_position = 0
 arc = tk.PhotoImage(file='arc_accueil.png')
 window.wm_attributes("-transparentcolor", "orange")
 
 
 def home():
     def start():
+        if arrow_number.get() < 1:
+            return
+
         for _ in range(arrow_number.get()):
             arrows_coordinates.append([])
         home_frame.destroy()
@@ -53,8 +57,11 @@ def overview():
         return len(arrows_coordinates[target_number]) == current_volley
 
     def open_target(target_number):
+        global overview_scroll_position
+
         if len(arrows_coordinates[target_number]) < current_volley:
             overview_frame.destroy()
+            overview_scroll_position = scroll_bar.get()[0]
             add_impact(target_number)
 
     def on_target_click(event, scroll_bar_offset):
@@ -147,6 +154,7 @@ def overview():
     )
     target_canvas.pack()
     scroll_bar.config(command=target_canvas.yview)
+    target_canvas.yview_moveto(overview_scroll_position)
     for i in range(arrow_number.get()):
         small_target(i)
     target_canvas.bind("<Button-1>", lambda event: on_target_click(event,
@@ -156,23 +164,60 @@ def overview():
                               state="normal" if is_finish_button_active() else "disabled")
     finish_button.grid(row=0, column=2)
 
-
 def add_impact(target_number):
-    def on_click(event):
-        global current_volley
-        arrows_coordinates[target_number].append((event.x/4, event.y/4))
+    global impact_to_add
+    impact_to_add = [None, None]
 
-        new_volley = True
-        for arrow in arrows_coordinates:
-            if len(arrow) < current_volley:
-                new_volley = False
-                break
-        if new_volley:
-            current_volley += 1
+    def on_target_click(event):
+        global impact_to_add
+        impact_to_add = [(event.x-30)/4, (event.y-30)/4]
+        draw_target()
+        canvas.create_oval(impact_to_add[0]*4+25, impact_to_add[1]*4+25, impact_to_add[0]*4+35, impact_to_add[1]*4+35, fill="green", outline="green")
+        finish_button.config(state="normal")
 
-        arrow_frame.destroy()
-        overview()
+    def on_button_click():
+        global current_volley, impact_to_add
 
+        if impact_to_add != [None, None]:
+            arrows_coordinates[target_number].append(impact_to_add)
+        
+            new_volley = True
+            for arrow in arrows_coordinates:
+                if len(arrow) < current_volley:
+                    new_volley = False
+                    break
+            if new_volley:
+                current_volley += 1
+
+            arrow_frame.destroy()
+            overview()
+
+    def draw_target():
+        canvas.delete("all")
+
+        if is_trispot.get():
+            canvas.create_oval(32, 32, 428, 428, fill="blue")
+            canvas.create_oval(72, 72, 388, 388, fill="red")
+            canvas.create_oval(110, 110, 350, 350, fill="red")
+            canvas.create_oval(150, 150, 310, 310, fill="yellow")
+            canvas.create_oval(190, 190, 270, 270, fill="yellow")
+            canvas.create_oval(210, 210, 250, 250, fill="yellow")
+        else:
+            canvas.create_oval(32, 32, 428, 428, fill="white")
+            canvas.create_oval(51, 51, 409, 409, fill="white")
+            canvas.create_oval(70, 70, 390, 390, fill="black")
+            canvas.create_oval(90, 90, 370, 370, fill="black", outline="white")
+            canvas.create_oval(110, 110, 350, 350, fill="blue")
+            canvas.create_oval(130, 130, 330, 330, fill="blue")
+            canvas.create_oval(150, 150, 310, 310, fill="red")
+            canvas.create_oval(170, 170, 290, 290, fill="red")
+            canvas.create_oval(190, 190, 270, 270, fill="yellow")
+            canvas.create_oval(210, 210, 250, 250, fill="yellow")
+            canvas.create_oval(220, 220, 240, 240, fill="yellow")
+
+        for x, y in arrows_coordinates[target_number]:
+            canvas.create_oval(x*4+25, y*4+25, x*4+35, y*4+35, fill="green", outline="green")
+        
     arrow_frame = ttk.Frame(window)
     arrow_frame.pack(fill="both")
 
@@ -189,41 +234,19 @@ def add_impact(target_number):
         menu_bar_content, text="Volée n°" + str(current_volley), bg="#3ED8FF")
     current_volley_label.grid(row=0, column=1, padx=40, pady=10)
 
-    finish_button = tk.Button(menu_bar_content, text="Valider",
-                              command=lambda _: print("hello"), state="normal")
+    finish_button = tk.Button(menu_bar_content, text="Valider", command=on_button_click, state="disabled")
     finish_button.grid(row=0, column=2)
 
-    canvas = tk.Canvas(arrow_frame, width=400, height=400)
-    canvas.pack(padx=10, pady=10)
-
-    if is_trispot.get():
-        canvas.create_oval(2, 2, 398, 398, fill="blue")
-        canvas.create_oval(42, 42, 358, 358, fill="red")
-        canvas.create_oval(80, 80, 320, 320, fill="red")
-        canvas.create_oval(120, 120, 280, 280, fill="yellow")
-        canvas.create_oval(160, 160, 240, 240, fill="yellow")
-        canvas.create_oval(180, 180, 220, 220, fill="yellow")
-    else:
-        canvas.create_oval(2, 2, 398, 398, fill="white")
-        canvas.create_oval(21, 21, 379, 379, fill="white")
-        canvas.create_oval(40, 40, 360, 360, fill="black")
-        canvas.create_oval(60, 60, 340, 340, fill="black", outline="white")
-        canvas.create_oval(80, 80, 320, 320, fill="blue")
-        canvas.create_oval(100, 100, 300, 300, fill="blue")
-        canvas.create_oval(120, 120, 280, 280, fill="red")
-        canvas.create_oval(140, 140, 260, 260, fill="red")
-        canvas.create_oval(160, 160, 240, 240, fill="yellow")
-        canvas.create_oval(180, 180, 220, 220, fill="yellow")
-        canvas.create_oval(190, 190, 210, 210, fill="yellow")
-
-    for x, y in arrows_coordinates[target_number]:
-        canvas.create_oval(x*4-5, y*4-5, x*4+5, y*4+5,
-                           fill="green", outline="green")
-
-    canvas.bind("<Button-1>", on_click)
+    canvas = tk.Canvas(arrow_frame, width=460, height=460)
+    canvas.pack()
+    draw_target()
+    
+    canvas.bind("<Button-1>", on_target_click)
 
 
 def stats():
+    arrows_classement = []
+
     def moyenne(list):
         somme_x = 0
         somme_y = 0
@@ -245,36 +268,43 @@ def stats():
         ecart_type_y = math.sqrt(somme_ecart_carree_y / len(list))
         return (ecart_type_x, ecart_type_y)
 
-    stats_frame = ttk.Frame(window)
-    stats_frame.pack()
+    def score(moyenne_score,ecart_type_score):
+        distance=math.sqrt((moyenne_score[0]-50)**2+(moyenne_score[1]-50)**2)
+        score_fleche=1000/(10*distance+(ecart_type_score[0]*ecart_type_score[1]))
+        score_final=float(score_fleche)
+        return (score_final)
 
-    rows = 0
-    columns = 0
+    stats_frame = ttk.Frame(window)
+    stats_frame.pack(fill='both')
 
     for i in range(len(arrows_coordinates)):
-        arrow_frame = ttk.Frame(stats_frame)
-        arrow_frame.grid(row=rows, column=columns)
+        moyenne_fleche = moyenne(arrows_coordinates[i])
+        ecart_type_fleche=ecart_type(arrows_coordinates[i])
+        arrows_classement.append([score(moyenne_fleche,ecart_type_fleche),i,moyenne_fleche,ecart_type_fleche])
+    arrows_classement.sort()
+    arrows_classement.reverse()
+    
+    scroll_bar_score=ttk.Scrollbar(stats_frame, orient='vertical')
+    scroll_bar_score.pack(side='right',fill='y')
+    can2=tk.Canvas(stats_frame,width=800,height=(len(arrows_classement)+1)*30,yscrollcommand=scroll_bar_score.set,scrollregion='0 0 800 '+str((len(arrows_classement)+1)*30))
+    can2.pack()
+    scroll_bar_score.config(command=can2.yview)
+    
+    can2.create_text(75,15,text="Flèche n°")
+    can2.create_text(225,15,text="Moyenne :")
+    can2.create_text(425,15,text="Ecart-type :")
+    can2.create_text(625,15,text="Score :")
+    can2.create_text(725,15,text="Classement :")
 
-        arrow_number_label = ttk.Label(
-            arrow_frame, text="Flèche n°" + str(i+1))
-        arrow_number_label.pack()
+    arrow_y = 3
 
-        impacts_label = ttk.Label(arrow_frame, text=str(arrows_coordinates[i]))
-        impacts_label.pack()
-
-        moyenne_label = ttk.Label(
-            arrow_frame, text="Moyenne : " + str(moyenne(arrows_coordinates[i])))
-        moyenne_label.pack()
-
-        ecart_type_label = ttk.Label(
-            arrow_frame, text="Ecart-tpe : " + str(ecart_type(arrows_coordinates[i])))
-        ecart_type_label.pack()
-
-        if rows == 4:
-            rows = -1
-            columns += 1
-        if rows < 4:
-            rows += 1
+    for i in range(len(arrows_classement)):
+        can2.create_text(75,15*arrow_y,text=str(arrows_classement[i][1]+1))
+        can2.create_text(225,15*arrow_y,text="x : " + str(arrows_classement[i][2][0]) + "   y : " + str(arrows_classement[i][2][1]))
+        can2.create_text(425,15*arrow_y,text="x : " + str(arrows_classement[i][3][0]) + "   y : " + str(arrows_classement[i][3][1]))
+        can2.create_text(625,15*arrow_y,text=str(arrows_classement[i][0]))
+        can2.create_text(725,15*arrow_y,text="#"+str(i+1))
+        arrow_y += 2
 
 
 home()
